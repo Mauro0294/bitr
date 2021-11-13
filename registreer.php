f106b4ac520243ffb8f1e5b7bc412b373c8ce995
Mauro Scheltens <mauroscheltens@hotmail.com>
2021-11-13 21:08:11 +0100

    Inlog & Registreer pagina responsive gemaakt met TailWindCSS

--- registreer.php
+++ registreer.php
@@ -11,32 +11,54 @@ session_start();
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="HandheldFriendly" content="true">
-    <link rel="stylesheet" type="text/css" href="css/registreer_bitr.css">
+    <link rel="stylesheet" type="text/css" href="tailwind.css">
+    <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+    <style>
+            body {
+                background-color: rgb(255, 255, 255);
+                font-weight: bold;
+                font-style: italic;
+                font-family: 'Roboto Mono', monospace;
+                background-repeat: no-repeat;
+                background-size: cover;
+                margin: 0px;
+                padding: 0px;
+                cursor: default;
+                background-image: url("images/gify1.gif");
+            }
+
+            * {
+                font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif;
+            }
+
+            input {
+                background-color: #2C2C2C;
+            }
+        </style>
 </head>
-<body id="body">
-<form method="POST">
-    <div id="login">
-
-    <div id="box1"></div>
-    
-    <img id="log" src="images/bitr2.png" alt="Jean logo">
-    <h1 id="h2">CREËER EEN ACCOUNT.</h1>
-
-    <label id="label1" for="name"><b>GEBRUIKERSNAAM.</b></label>
-    <input id="usern" type="text" placeholder="Gebruikersnaam" name="gebruikersnaam" maxlength="25"  required>
-
-    <label id="label2" for="name2"><b>WACHTWOORD.</b></label>
-    <input id="passw" type="password" placeholder="Wachtwoord" name="wachtwoord" required>
-
-    <label id="label3" for="name2"><b>E-MAIL.</b></label>
-    <input id="email" type="email" placeholder="Email" name="email" required>
-
-    <button class="submit" name="registreren">REGISTREREN.</button>
-    </div>
-</form>
-
-<p id="registreer">HEB JE AL EEN ACCOUNT?   <a id="href" href='login.php'>LOGIN!</a></span></p>
-</body>
+<body class='overflow-hidden'>
+        <div class='float-right lg:w-2/5 sm:w-full h-full bg-black lg:mt-0 sm:pt-80 lg:pt-0 text-white block text-center' data-aos="fade-left" data-aos-duration="500">
+            <div class='flex justify-center'>
+                <img src='images/logo.png' class='lg:w-10 sm:w-32 mt-14'>
+            </div>
+            <h1 class='lg:text-3xl sm:text-7xl lg:mt-8 sm:mt-4 mb-14'>CREËER EEN ACCOUNT.</h1>
+            <form method='post'>
+                <h1 class='mb-2 lg:text-lg sm:text-3xl'>GEBRUIKERSNAAM.</h1>
+                <input type='text' name='gebruikersnaam' class='sm:w-1/2 mb-8 lg:py-4 lg:pr-20 lg:text-lg sm:text-2xl sm:py-12 sm:pr-32 outline-none pl-2 rounded' placeholder='Gebruikersnaam' maxlength='25' required>
+                <h1 class='mb-2 lg:text-lg sm:text-3xl'>EMAIL.</h1>
+                <input type='text' name='gebruikersnaam' class='sm:w-1/2 mb-8 lg:py-4 lg:pr-20 lg:text-lg sm:text-2xl sm:py-12 sm:pr-32 outline-none pl-2 rounded' placeholder='Email' maxlength='25' required>
+                <h1 class='mb-2 lg:text-lg sm:text-3xl'>WACHTWOORD.</h1>
+                <input name='wachtwoord' class='sm:w-1/2 mb-8 lg:py-4 lg:pr-20 lg:text-lg sm:text-2xl sm:py-12 sm:pr-32 outline-none pl-2 rounded' placeholder='Wachtwoord' type='password' required><br>
+                <button name='login' class='bg-blue-600 rounded-full px-20 py-2 mb-10'>INLOGGEN.</button>
+            </form>
+            <div class='h-10 w-full bg-black lg:hidden sm:block'>
+        </div>
+        <h1 class='lg:text-xs sm:text-2xl mb-20'>HEB JE AL EEN ACCOUNT?<a href='login.php' class='text-blue-500 hover:text-blue-600 duration-500'> LOGIN!</a></h1>
+        <img src='images/gify1.gif' class='lg:hidden sm:block w-full h-1/2 mr-10'>
+        </div>
+        <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+        <script src='data-aos.js'></script>
+    </body>
 
 <?php
   if (isset($_POST['registreren'])) {
